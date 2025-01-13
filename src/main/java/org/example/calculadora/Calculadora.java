afbf6afb35ed4bf48e5ece974621f13455119e81
Adrian Polo <adrianpoloper@gmail.com>
2025-01-13 10:10:49 +0100

    calculadora

---
+++ src/main/java/org/example/calculadora/Calculadora.java
@@ -0,0 +1,64 @@
+package org.example.calculadora;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "calculadora", value = "/calculadora")
+public class Calculadora extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello Calculadora!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Recoger datos del formulario
+        Integer num1 = request.getParameter("num1") != "" ? Integer.valueOf(request.getParameter("num1")) : null;
+        Integer num2 = request.getParameter("num2") != "" ? Integer.valueOf(request.getParameter("num2")) : null;
+        String operador = request.getParameter("operador");
+
+        // Enviar respuesta al cliente
+        response.setContentType("text/html");
+        switch (operador){
+            case "+":
+                suma(num1, num2);
+                break;
+            case "-":
+                resta(num1, num2);
+                break;
+            case "*":
+                multiplicacion(num1, num2);
+                break;
+            case "/":
+                division(num1, num2);
+                break;
+            default:
+                response.getWriter().print("No va");
+                break;
+        }
+    }
+
+    public static Integer suma(Integer num1, Integer num2) {
+        return num1 + num2;
+    }
+
+    public static Integer resta(Integer num1, Integer num2) {
+        return num1 - num2;
+    }
+
+    public static Integer multiplicacion(Integer num1, Integer num2) {
+        return num1 * num2;
+    }
+
+    public static Integer division(Integer num1, Integer num2) {
+        return num1 / num2;
+    }
+
+    public void destroy() {
+    }
+}
